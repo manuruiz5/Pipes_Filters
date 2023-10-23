d5fdc2e9a5c1cfda4c4b89f3ae5090e8bd70e210
manuruiz5 <manuela.ruiz@correo.ucu.edu.uy>
2023-10-23 10:33:02 -0300

    parte 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,18 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            //Parte 1:carga una imagen, crea instancias de filtros, crea instacnias de pipes
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"PathToImageToLoad.jpg");
+
+            IFilter greyscaleFilter= new FilterGreyscale();
+            IFilter negativeFilter=new FilterNegative();
+            IPipe pipe1=new PipeSerial(greyscaleFilter, new PipeNull());
+            IPipe pipe2=new PipeSerial(negativeFilter,new PipeNull());
+
+            IPicture image=pipe1.Send(picture);
+            IPicture finalImage=pipe2.Send(image);
+
         }
     }
 }
